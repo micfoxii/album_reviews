5a995e6acf36407de8e49257bce6d035323364f1
Mic Fox <michael.c.fox.ii@gmail.com>
2019-07-08 16:03:55 -0400

    add description

---
+++ lib/album_reviews.rb
@@ -0,0 +1,6 @@
+require "album_reviews/version"
+
+module AlbumReviews
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/album_reviews_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe AlbumReviews do
+  it "has a version number" do
+    expect(AlbumReviews::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
